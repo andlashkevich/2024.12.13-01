e22f59e2d1a1d0e96d9413464c4ca97c54667b07
Andrey Lashkevich <andlashkevich@ya.ru>
2024-12-18 04:09:44 +0300

    2

--- components/task2/task2.js
+++ components/task2/task2.js
@@ -4,41 +4,83 @@ import styles from "./task2.module.css";
 import { useEffect, useState } from "react";
 
 export function Task2() {
-	const tasks = [
-		"Pay bills",
-		"Meet George",
-		"By eggs",
-		"Read a book",
-		"Organize office",
-	];
 	const [inTask, setInTask] = useState("");
-	const [outTask, setOutTask] = useState(tasks);
+	const [outTask, setOutTask] = useState([]);
 	const [error, setError] = useState(null);
+	const [block, setBlock] = useState(false);
+	// const refreshFlag = () => setRefresh(!refresh)
+
 	const inputChange = ({ target }) => {
 		let error = null;
 		setInTask(target.value);
-		if (target.value.length > 10) error = "Большая задача";
+		if (outTask.find((it) => it.task === target.value))
+			error = "Такая задача уже существует";
+		if (target.value.length > 30) error = "Слишком длинная задача";
 		setError(error);
 	};
+	useEffect(() => {
+		fetch("http://localhost:3003/tasks")
+			.then((rsp) => rsp.json())
+			.then((dt) => {
+				setOutTask(dt);
+			});
+	}, [block]);
 
 	const createTask = () => {
-		tasks.unshift(inTask);
-		setOutTask(tasks);
-		console.log(outTask);
-		setInTask("");
-		// fetch("http://localhost:3005/tasks", {
-		// 	method: "POST",
-		// 	headers: { "Content-Type": "application/json; charset=utf8" },
-		// 	body: JSON.stringify({ tasks: inTask }),
-		// })
-		// 	.then((resp) => resp.json())
-		// 	.then((response) => {
-		// 		console.log("Добавлено, ответ сервера:", response);
-		// 	});
+		setBlock(true);
+		fetch("http://localhost:3003/tasks", {
+			method: "POST",
+			headers: { "Content-Type": "application/json; charset=utf-8" },
+			body: JSON.stringify({ task: inTask }),
+		})
+			.then((rsp) => rsp.json())
+			.then((dt) => console.log(dt))
+			.catch((error) => console.log(error))
+			.finally(() => {
+				setInTask("");
+				setBlock(false);
+			});
 	};
+
+	const delTask = (e) => {
+		let id = outTask.find((it) => {
+			if (it.task === e.target.previousElementSibling.innerText)
+				return it;
+		}).id;
+		fetch(`http://localhost:3003/tasks/${id}`, {
+			method: "DELETE",
+		});
+	};
+
+	const sortTask = (e) => {
+		e.preventDefault();
+		let sTask = [...outTask];
+		setOutTask(
+			sTask.sort((a, b) =>
+				a.task.toLowerCase() > b.task.toLowerCase() ? 1 : -1,
+			),
+		);
+	};
+
+	const changeTask = (e) => {
+		e.preventDefault();
+		console.log(e);
+	};
+
+	const findTask = () => {
+		inTask
+			? outTask.find((it) => {
+					it.task.includes(inTask);
+					let m = `Задача № ${it.task.id + 1} соответсвует запросу`;
+					return m;
+				})
+			: setError("Поиск невозможен");
+	};
+
 	return (
 		<form className={styles.wrap}>
 			<h1 className={styles.head}> Задачи на неделю</h1>
+			{error ? <div className={styles.error}>{error}</div> : null}
 			<textarea
 				name="newTask"
 				value={inTask}
@@ -47,14 +89,31 @@ export function Task2() {
 				className={styles.input}
 				placeholder="Введите новую задачу или нажмите на существующую для внесения изменений"
 				autoFocus={true}
+				required={true}
 				autoComplete="on"
 			></textarea>
 			<div className={styles.buttons}>
-				<button className={styles.сButton} onClick={createTask}>
+				<button
+					disabled={block}
+					className={styles.сButton}
+					onClick={createTask}
+				>
 					Добавить
 				</button>
-				<button className={styles.oButton}>Упорядочить</button>
-				<button className={styles.fButton}>Найти</button>
+				<button
+					disabled={block}
+					onClick={sortTask}
+					className={styles.oButton}
+				>
+					Упорядочить
+				</button>
+				<button
+					disabled={block}
+					onClick={findTask}
+					className={styles.fButton}
+				>
+					Найти
+				</button>
 			</div>
 			<ol className={styles.ol}>
 				{outTask.map((it, id) => {
@@ -62,12 +121,19 @@ export function Task2() {
 						<div className={styles.tasks} key={id}>
 							<li className={styles.uncheck}>
 								<input
+									onChange={changeTask}
 									className={styles.checkbox}
 									type={"checkbox"}
 								></input>
-								{it}
+								{it.task}
 							</li>
-							<button className={styles.del}>Удалить</button>
+							<button
+								disabled={block}
+								onClick={delTask}
+								className={styles.del}
+							>
+								Удалить
+							</button>
 						</div>
 					);
 				})}
