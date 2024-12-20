d45d2a6527fbfd9c710b644f653b251215296b43
Andrey Lashkevich <andlashkevich@ya.ru>
2024-12-20 16:48:31 +0300

    8

---
+++ next.config.js
@@ -0,0 +1,11 @@
+const nextConfig = {
+	distDir: "build",
+	output: "export",
+	reactStrictMode: true,
+	// swcMinify: true,
+	// images: {
+	// 	unoptimized: true,
+	// },
+};
+
+module.exports = nextConfig;
